dadb1a983fd5493473500fc76e2316ff714fa467
kaori.iida <pelikan_singer_5@yahoo.co.jp>
2021-07-21 10:39:28 +0900

    Initial Commit

---
+++ TaskP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskP
+//
+//  Created by イイダカオリ on 2021/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
